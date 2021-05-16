446b0b63d8e73c52ed2685d674abac6356ae75bc
rick12000 <riccardodoyle14@gmail.com>
2021-05-17 00:33:05 +0100

    initial code and set up files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'time-cross-validation',
+    version = '0.0.1',
+    description = 'Time series cross validation tool, supplementing or replacing relevant existing sklearn libraries.',
+    url = '',
+    author = 'Riccardo Doyle',
+    #py_modules = ["time_cross_validation"],
+    author_email = 'riccardo.doyle.14@ucl.ac.uk',
+    keywords = 'cross validation',
+    packages = find_packages()
+)
